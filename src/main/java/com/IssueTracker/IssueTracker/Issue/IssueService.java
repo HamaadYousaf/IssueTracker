ac9937e12498b11a60b7b0e475aef8c52e5b029d
HamaadYousaf <hamaaadyousaf@outlook.com>
2024-05-11 17:51:09 -0400

    get/update/delete issue by id

--- src/main/java/com/IssueTracker/IssueTracker/Issue/IssueService.java
+++ src/main/java/com/IssueTracker/IssueTracker/Issue/IssueService.java
@@ -3,6 +3,7 @@ package com.IssueTracker.IssueTracker.Issue;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -34,5 +35,36 @@ public class IssueService {
 
     public boolean issueDoesNotExists(Integer id) {
         return !issueRepository.existsById(id);
+    }
+
+    public Optional<IssueEntity> getIssueById(Integer id) {
+        return issueRepository.findById(id);
+    }
+
+    public IssueEntity updateIssue(IssueEntity issueEntity, Integer id) {
+        return issueRepository
+                .findById(id)
+                .map(
+                        existingIssue -> {
+                            existingIssue.setId(id);
+                            Optional.ofNullable(issueEntity.getTitle())
+                                    .ifPresent(existingIssue::setTitle);
+                            Optional.ofNullable(issueEntity.getDescription())
+                                    .ifPresent(existingIssue::setDescription);
+                            Optional.ofNullable(issueEntity.getPriority())
+                                    .ifPresent(existingIssue::setPriority);
+                            Optional.ofNullable(issueEntity.getStatus())
+                                    .ifPresent(existingIssue::setStatus);
+                            Optional.ofNullable(issueEntity.getCategory())
+                                    .ifPresent(existingIssue::setCategory);
+                            Optional.ofNullable(issueEntity.getAssignedTo())
+                                    .ifPresent(existingIssue::setAssignedTo);
+                            return issueRepository.save(existingIssue);
+                        })
+                .orElseThrow(() -> new RuntimeException("Issue does not exist"));
+    }
+
+    public void deleteIssueById(Integer id) {
+        issueRepository.deleteById(id);
     }
 }
